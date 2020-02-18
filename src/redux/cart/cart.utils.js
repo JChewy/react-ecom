89928560bd5c4d3f552c34fa63c0514fe8e4e53e
jeff <retroh3at@gmail.com>
2020-02-18 07:12:47 -0500

    created cart item component and connected cart dropdown with cart item reducer. also fixed additemtocart bug

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,18 +1,15 @@
 export const addItemToCart = (cartItems, cartItemToAdd) => {
-
-    //loops through cart array. possible future update to make
-    //more efficient would be to use hashtable instead.
-    //probably ok for now as cart is probably not going to be thousands of items large. 
     const existingCartItem = cartItems.find(
-        cartItem => cartItem.id === cartItem.id
+        cartItem => cartItem.id === cartItemToAdd.id
     );
 
     if (existingCartItem) {
         return cartItems.map(cartItem =>
-            cartItem.id === cartItemToAdd.id ? { ...cartItem, quantity: cartItem.quantity + 1 }
+            cartItem.id === cartItemToAdd.id
+                ? { ...cartItem, quantity: cartItem.quantity + 1 }
                 : cartItem
-        )
+        );
     }
 
-    return [...cartItems, { ...cartItemToAdd, quantity: 1 }]
-}
+    return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+};
